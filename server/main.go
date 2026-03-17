d6d5a66dcc1c13b3e1e735634db1ff496b84b5a3
kuche1 <znamsiparolata4esno@abv.bg>
2026-03-17 22:55:55 +0200

    update

--- server/main.go
+++ server/main.go
@@ -4,12 +4,8 @@ package server
 
 import (
 	"context"
-	"errors"
-	"io/fs"
 	"log"
-	"os"
 
-	"github.com/kuche1/cloud-note/lib"
 	"github.com/quic-go/quic-go"
 )
 
@@ -39,76 +35,6 @@ func handleNewConnections(listener *quic.Listener) {
 		log.Printf("Handling connection...")
 		handleConnection(conn)
 
-		// conn.CloseWithError(0, "")
-
 		log.Printf("Connection handled!")
 	}
-}
-
-func handleConnection(conn *quic.Conn) {
-	// TODO: Make some mechanism for automatically sending errors relevant to the client
-
-	if _, err := os.Stat(NoteFile); err != nil {
-		if errors.Is(err, fs.ErrNotExist) {
-			err = os.WriteFile(NoteFile, []byte{}, 0644)
-			if err != nil {
-				log.Printf("Could not create initial note file: %v", err)
-				return
-			}
-		} else {
-			log.Printf("Could not check for note file's existance: %v", err)
-			return
-		}
-	}
-
-	// IMPROVE: Read the file by chunks
-	data, err := os.ReadFile(NoteFile)
-	if err != nil {
-		log.Printf("Could not read note: %v", err)
-		return
-	}
-
-	// TODO: Use the nonblocking version
-	stream, err := conn.OpenStreamSync(context.Background())
-	if err != nil {
-		log.Printf("Could not open stream: %v", err)
-		return
-	}
-
-	// TODO: Add a timeout
-	err = lib.SendDatalenSliceByte(stream, data)
-	if err != nil {
-		log.Printf("Clould not send note content: %v", err)
-		return
-	}
-
-	log.Printf("Getting new note content...")
-
-	// TODO: Add a timeout
-	data, err = lib.RecvDatalenSliceByte(stream)
-	if err != nil {
-		log.Printf("Could not receive note content: %v", err)
-		return
-	}
-
-	log.Printf("Got new note content")
-
-	log.Printf("Sending EOF")
-
-	err = lib.SendEOF(stream)
-	if err != nil {
-		log.Printf("Could not send EOF: %v", err)
-		return
-	}
-
-	log.Printf("Sent EOF")
-
-	err = os.WriteFile(NoteFile, data, 0644)
-	if err != nil {
-		log.Printf("Could not write note: %v", err)
-		// TODO: This definetely needs to be sent to the client
-		return
-	}
-
-	log.Printf("Wrote new note content")
 }
