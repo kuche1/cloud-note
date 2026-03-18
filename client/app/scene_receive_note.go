f50365c9073c84d5f82f5bea963a43a1b81614a3
kuche1 <znamsiparolata4esno@abv.bg>
2026-03-18 19:38:33 +0200

    update

--- client/app/scene_receive_note.go
+++ client/app/scene_receive_note.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
 	"github.com/kuche1/cloud-note/client/action"
 	"github.com/kuche1/cloud-note/client/settings"
@@ -9,17 +10,28 @@ import (
 
 func (self *App) SceneReceiveNote(settings *settings.Settings) {
 	output := widget.NewTextGrid()
-	self.window.SetContent(output)
 
-	go receiveNote(self, output, settings)
+	dialog := dialog.NewCustomWithoutButtons(
+		"Receving Note",
+		output,
+		*self.window.FyneWindow,
+	)
+
+	dialog.Show()
+
+	go receiveNote(self, settings, output, dialog)
 }
 
-func receiveNote(app *App, output *widget.TextGrid, settings *settings.Settings) {
+func receiveNote(app *App, settings *settings.Settings, output *widget.TextGrid, dialog *dialog.CustomDialog) {
 	data, err := action.ActionGetNoteContent(app.window.FyneWindow, output, settings)
 	if err != nil {
+		dialog.Dismiss()
 		app.ScenePanic(err.Error())
 		return
 	}
 
-	fyne.Do(func() { app.SceneEditNote(string(data), settings) })
+	fyne.Do(func() {
+		dialog.Dismiss()
+		app.SceneEditNote(string(data), settings)
+	})
 }
