3b060563b852d7bfaf0234ac3899914728028adc
kuche1 <znamsiparolata4esno@abv.bg>
2026-03-20 16:06:28 +0200

    update

--- client/config/config.go
+++ client/config/config.go
@@ -1,10 +1,7 @@
 package config
 
-import "math"
-
 // These values can be lowered if you suspect that the server wants
 // to flood your RAM
-// TODO: Add some better defaults
-const NoteNameMaxLength uint64 = math.MaxUint64
-const NoteContentsMaxLength uint64 = math.MaxUint64
-const NumberOfNotesMaxLength uint64 = math.MaxUint64
+const NoteNameMaxLength uint64 = 512 * 2
+const NoteContentsMaxLength uint64 = 1024 * 1024 * 2 * 2
+const NumberOfNotesMaxLength uint64 = 512
