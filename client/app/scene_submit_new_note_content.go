5c6993f4d433209d6ae61a68c8e521abfecb9418
kuche1 <znamsiparolata4esno@abv.bg>
2026-04-01 16:07:18 +0300

    remove fixed comment

--- client/app/scene_submit_new_note_content.go
+++ client/app/scene_submit_new_note_content.go
@@ -12,9 +12,6 @@ import (
 // BUT if we are to do that we need to make that a setting as to let paranoid users send
 // the same note a billion time (perhaps add a setting for this, and when the button is clicked,
 // perform a check weather the content has changed or not)
-// [TODO: fix in progress] IMPROVE000: For some reason after we switch back to the previous scene the window expands
-// dramatically (vertically) on desktop; and this happens exclusively after having pressed the
-// submit button
 func (self *App) SceneSubmitNewNoteContent(
 	newText string,
 	noteName string,
